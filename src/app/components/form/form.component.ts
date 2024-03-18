dc45e10094f80a354480d7def45e27f302a374de
marleypm16 <yarlempm16@gmail.com>
2024-03-18 11:10:06 -0300

    Validação telefone

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,90 +1,72 @@
 import { CommonModule } from '@angular/common';
-import { Component,OnInit } from '@angular/core';
-import {  FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { Router } from '@angular/router';
 import { User } from '../../user';
 
-
-type showError = 'show' | 'notShow'
+type showError = 'show' | 'notShow';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [ReactiveFormsModule,CommonModule,HeaderComponent,FooterComponent],
+  imports: [ReactiveFormsModule, CommonModule, HeaderComponent, FooterComponent],
   templateUrl: './form.component.html',
-  styleUrl: './form.component.css'
+  styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit  {
-    usuario! : User;
-    
-    profileForm! :FormGroup;
-    errorMessage : showError = 'notShow'
-    pageActive : string = ''
-    
+export class FormComponent implements OnInit {
+  usuario!: User;
 
-    constructor(
-      private router: Router
+  profileForm!: FormGroup;
+  errorMessage: showError = 'notShow';
+  pageActive: string = '';
 
-      ){
+  constructor(private router: Router) {}
 
-      }
-    ngOnInit() : void {
-      const usuarioString = localStorage.getItem('user');
-      if (usuarioString){
-        this.usuario = JSON.parse(usuarioString!) as User;
-      }else{
-        this.usuario = {name :'',email:'',phoneNumber:''}
-      }
-      this.profileForm = new FormGroup({
-        user: new FormControl(this.usuario.name,[Validators.required,Validators.maxLength(15),Validators.minLength(5)]),
-        email : new FormControl(this.usuario.email,[Validators.email,Validators.required]),
-        phoneNumber : new FormControl(this.usuario.phoneNumber,[Validators.required,Validators.minLength(11)])
-      })
-
+  ngOnInit(): void {
+    const usuarioString = localStorage.getItem('user');
+    if (usuarioString) {
+      this.usuario = JSON.parse(usuarioString) as User;
+    } else {
+      this.usuario = { name: '', email: '', phoneNumber: '' };
     }
+    this.profileForm = new FormGroup({
+      user: new FormControl(this.usuario.name, [Validators.required, Validators.maxLength(15), Validators.minLength(5)]),
+      email: new FormControl(this.usuario.email, [Validators.email, Validators.required]),
+      phoneNumber: new FormControl(this.usuario.phoneNumber, [Validators.required, this.phoneNumberValidator])
+    });
+  }
 
-   
+  get user() {
+    return this.profileForm.get('user')!;
+  }
+  get email() {
+    return this.profileForm.get('email')!;
+  }
+  get phoneNumber() {
+    return this.profileForm.get('phoneNumber')!;
+  }
 
-    get user(){
-      return this.profileForm.get('user')!
+  submit() {
+    if (this.profileForm.invalid) {
+      this.errorMessage = 'show';
+      return;
     }
-    get email(){
-      return this.profileForm.get('email')!
-    }
-    get phoneNumber(){
-      return this.profileForm.get('phoneNumber')!
-    }
+    const user: User = {
+      name: this.user.value,
+      email: this.email.value,
+      phoneNumber: this.phoneNumber.value
+    };
+    localStorage.setItem('user', JSON.stringify(user));
+    this.router.navigate(['form2']);
+  }
 
-    phoneValidator() : boolean{
-      if(this.phoneNumber.value == 0){
-        return true
-      }
-      else{
-        return false
-      } 
+  phoneNumberValidator(control: FormControl): { [s: string]: boolean } | null {
+    const phoneNumberPattern = /^[0-9]{11}$/;
+    if (!phoneNumberPattern.test(control.value)) {
+      return { invalidPhoneNumber: true };
     }
-
-    submit(){
-      if(this.profileForm.invalid ){
-        this.errorMessage = 'show'
-        return
-      }
-      if(this.phoneValidator()){
-        this.errorMessage = 'show'
-        return
-
-      }
-      const user : User ={
-        name : this.user.value,
-        email : this.email.value,
-        phoneNumber : this.phoneNumber.value
-      }
-      localStorage.setItem('user',JSON.stringify(user))
-      this.router.navigate(['form2'])
-
-      
-    }
-    
+    return null;
+  }
 }
